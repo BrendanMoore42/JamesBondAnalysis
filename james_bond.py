e84e33b8aa09de38729672e5d9040c363d946a21
Brendan Moore <brendanm42@gmail.com>
2019-03-02 03:17:31 -0500

    Init commit--transfer to OSX

---
+++ james_bond.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env
+"""
+author: @BrendanMoore42
+Date: 02/03/2019
+
+Looking at the sentiment values for every james bond movie. OH baby.
+
+Checks overall sentiment over time in each movie holy shit great idea.
+
+Set srt file to dataframe, analyse dataframe
+
+Run from command line, example: James Bond - Goldfinger
+    $ python txt_to_cloud.py goldfinger.srt jamesbond.jpg
+If error, change encoding as 4th argument:
+    $ python txt_to_cloud.py goldfinger.srt jamesbond.jpg cp1252
+"""
